b6e544241ae5875ab79d0b3577539b9077ceb143
Ozcan Orhan Demirci <ozcanorhandemirci@ogr.iu.edu.tr>
2023-01-24 02:15:26 +0300

    Hello GitHub , Hello World

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace HelloGitHub
             Console.WriteLine();
             Console.WriteLine("Oyun ve Uygulama Akademisi bursiyeriyim. İzmir Bakırçay Üniversitesi Yönetim Bilişim Sistemleri bölümünde öğrenciyim.");
             Console.WriteLine("İkinci üniversite kapsamında İstanbul Üniversitesi Bilgisayar Programcılığı açıköğretim-önlisans öğrencisiyim.");
-            Console.WriteLine("Kendimi bildim bileli bilgisayar ile vakit geçiriyorum. Küçük yaşta bilgisayar ve internet ile tanışmam sayesinde sektörün gelişimini yakından takip edebildim");
+            Console.WriteLine("Kendimi bildim bileli bilgisayar ile vakit geçiriyorum. Küçük yaşta bilgisayar ve internet ile tanışmam sayesinde sektörün gelişimini yakından takip edebildim.");
             Console.WriteLine("Zaman içerisinde yazılım dünyasında yer almayı ve oyun geliştirmeyi hedefleyen bir genç haline geldim.");
             Console.WriteLine();
             Console.WriteLine("Kendimi öncelik sırasına göre şu alanlarda geliştiriyorum:");
@@ -32,8 +32,8 @@ namespace HelloGitHub
             Console.WriteLine("Kısaca hobilerimden ve ilgi alanlarımdan bahsedecek olursam... İmkan buldukça yüzmeyi ve su altını keşfetmeyi seviyorum. Türkiye Hisse Senedi Piyasasını (BIST100) takip ediyor ve yatırım yapıyorum.");
             Console.WriteLine("");
             Console.WriteLine("Son olarak Oyun ve Uygulama Akademisi ile Coursera eğitim programından bahsetmek istiyorum.");
-            Console.WriteLine("Oyun ve Uygulama Akademisi: Google Türkiye, Sanayi ve Teknoloji Bakanlığı, Cumhurbaşkanlığı Dijital Dönüşüm Ofisi'nin desteği, Girişimcilik Vakfı ve T3 Girişim Merkezi'nin iş birliği ile hayata geçmiş bir eğitim projesidir");
-            Console.WriteLine("Akademi kapsamında Google ve Coursera iş birliği ile hazırlanan Proje Yönetiminin Temelleri Uluslararası sertifika programına dahilim");
+            Console.WriteLine("Oyun ve Uygulama Akademisi: Google Türkiye, Sanayi ve Teknoloji Bakanlığı, Cumhurbaşkanlığı Dijital Dönüşüm Ofisi'nin desteği, Girişimcilik Vakfı ve T3 Girişim Merkezi'nin iş birliği ile hayata geçmiş bir eğitim projesidir.");
+            Console.WriteLine("Akademi kapsamında Google ve Coursera iş birliği ile hazırlanan Proje Yönetiminin Temelleri Uluslararası sertifika programına dahilim.");
             Console.WriteLine("Akademi sayesinde aldığım eğitimler: Unity (C#) eğitimi, Oyun Sanatı eğitimi (Modelleme ve Animasyon) , B2 İngilizce eğitimi ve Coursera Eğitimi (Sertifika Programı)");
             Console.WriteLine("");
             Console.WriteLine("Hazırladığım bu konsol uygulamasını incelediğiniz için teşekkür ederim.");
