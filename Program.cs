408958e2a1fd63bb92d9d51ef0dd9ea217becae0
Ozcan Orhan Demirci <ozcanorhandemirci@ogr.iu.edu.tr>
2023-01-24 02:08:53 +0300

    Hello GitHub , Hello World

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("internette paylaşacağım ilk programım için en iyi giriş bu olacak sanırım :D");
+            Console.WriteLine("Merhaba, ben Özcan Orhan Demirci. Bu konsol uygulamasını kendimi tanıtmak için yazıyorum.");
+            Console.WriteLine("GitHub hesabımı 23 Aralık 2022 tarihinde açtım ve üzerinden 1 ay geçti. Bugün 24 Ocak 2023. Bu 1 aylık süreç verimli ve keyifli geçti.");
+            Console.WriteLine("Temel programlama derslerini bitirmiş ve döngüler, eğerler, fonksiyonlar gibi konularda kendimi geliştirmiş olsam da bu programımda sadece kendimi tanıtacağım.");
+            Console.WriteLine();
+            Console.WriteLine("Oyun ve Uygulama Akademisi bursiyeriyim. İzmir Bakırçay Üniversitesi Yönetim Bilişim Sistemleri bölümünde öğrenciyim.");
+            Console.WriteLine("İkinci üniversite kapsamında İstanbul Üniversitesi Bilgisayar Programcılığı açıköğretim-önlisans öğrencisiyim.");
+            Console.WriteLine("Kendimi bildim bileli bilgisayar ile vakit geçiriyorum. Küçük yaşta bilgisayar ve internet ile tanışmam sayesinde sektörün gelişimini yakından takip edebildim");
+            Console.WriteLine("Zaman içerisinde yazılım dünyasında yer almayı ve oyun geliştirmeyi hedefleyen bir genç haline geldim.");
+            Console.WriteLine();
+            Console.WriteLine("Kendimi öncelik sırasına göre şu alanlarda geliştiriyorum:");
+            Console.WriteLine("1) Unity ve C#");
+            Console.WriteLine("2) Oyun ve bölüm dizaynı");
+            Console.WriteLine("3) Modelleme ve Animasyon");
+            Console.WriteLine("4) Proje Yönetimi ve Liderlik");
+            Console.WriteLine("5) Ses (Efekt) ve Müzik");
+            Console.WriteLine("Tabiki zaman içerisinde bu saydığım alanlardan bazılarına daha fazla önem vermeyi düşünüyorum. Şuan için kendimi keşfetme aşamasındayım.");
+            Console.WriteLine("");
+            Console.WriteLine("Kısaca hobilerimden ve ilgi alanlarımdan bahsedecek olursam... İmkan buldukça yüzmeyi ve su altını keşfetmeyi seviyorum. Türkiye Hisse Senedi Piyasasını (BIST100) takip ediyor ve yatırım yapıyorum.");
+            Console.WriteLine("");
+            Console.WriteLine("Son olarak Oyun ve Uygulama Akademisi ile Coursera eğitim programından bahsetmek istiyorum.");
+            Console.WriteLine("Oyun ve Uygulama Akademisi: Google Türkiye, Sanayi ve Teknoloji Bakanlığı, Cumhurbaşkanlığı Dijital Dönüşüm Ofisi'nin desteği, Girişimcilik Vakfı ve T3 Girişim Merkezi'nin iş birliği ile hayata geçmiş bir eğitim projesidir");
+            Console.WriteLine("Akademi kapsamında Google ve Coursera iş birliği ile hazırlanan Proje Yönetiminin Temelleri Uluslararası sertifika programına dahilim");
+            Console.WriteLine("Akademi sayesinde aldığım eğitimler: Unity (C#) eğitimi, Oyun Sanatı eğitimi (Modelleme ve Animasyon) , B2 İngilizce eğitimi ve Coursera Eğitimi (Sertifika Programı)");
+            Console.WriteLine("");
+            Console.WriteLine("Hazırladığım bu konsol uygulamasını incelediğiniz için teşekkür ederim.");
+            Console.WriteLine("");
+            Console.WriteLine("https://www.linkedin.com/in/ozcan-orhan-demirci");
+            Console.ReadLine();
+        }
+    }
+}
